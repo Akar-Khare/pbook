e8486f1ad4f69b8da912006a3e68d8fe47fd5c5c
Akar Khare <akarkhare02@gmail.com>
2023-04-27 21:13:25 +0530

    27/4 commit

--- index.js
+++ index.js
@@ -5,18 +5,15 @@ const port = 5000;
 const app = express();
 
 
-app.use(cors({
+app.use(cors());
 
-  origin: "https://pictbook69.netlify.app",
-  'Access-Control-Allow-Credentials': true
-}));
-
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "https://pictbook69.netlify.app");
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://pictbook69.netlify.app");
+  res.header('Access-Control-Allow-Credentials', true);
 //   res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
 //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next(); 
-// });
+  next(); 
+});
 
 app.use(require('./router/auth'));
 
