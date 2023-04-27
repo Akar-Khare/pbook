8ffc7112b89440f2fb0b36c216818ad5a2f7006d
Akar Khare <akarkhare02@gmail.com>
2023-04-27 20:51:06 +0530

    26/4 commit

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const port = 5000;
 const app = express();
 
 
-app.use(cors({
+app.use(cors());
 
-
-  origin: "http://localhost:3000",
-  'Access-Control-Allow-Credentials':'true'
-  
-  
-}));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next(); 
+});
 
 app.use(require('./router/auth'));
 
