55852a132eaa26144c70a4fc6adbcaaeaa56daa5
Akar Khare <akarkhare02@gmail.com>
2023-05-07 13:06:48 +0530

    06/5 commit

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ app.use(cors({
   origin: ['https://pictbook69.netlify.app','https://pictbook.onrender.com'],
   credentials: true,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  preflightContinue: true,
-  allowedHeaders:'*'
   
   
   }));
