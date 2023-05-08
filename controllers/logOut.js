db8d56117f1371c55de358385811c82aaf54c8d6
Akar Khare <akarkhare02@gmail.com>
2023-05-08 11:33:49 +0530

    08/5 commit

--- controllers/logOut.js
+++ controllers/logOut.js
@@ -1,7 +1,7 @@
 exports.logOut = ((req, res) => {
 
     
-    res.clearCookie('jwtoken',{path:'/',sameSite:'none'});
+    res.clearCookie('jwtoken',{path:'/',sameSite:'none',secure:true});
     res.status(200).send("Logout successful")
 
 })
