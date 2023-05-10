194e1560b80624eefbe1fa231bba04f8f23e2fab
Akar Khare <akarkhare02@gmail.com>
2023-05-10 14:56:14 +0530

    10/5 commit

--- schema/CardSchema.js
+++ schema/CardSchema.js
@@ -6,7 +6,7 @@ const cardSchema = new mongoose.Schema({
     userName: String,
     card:{
             key: String,
-            link :  String,
+            link : String,
             name: String,
             desc: String,
             likesCount : {type: Number, default:0},
